70c2ca5db8298a568ff72069005e5e44c66faa86
maxn <maxnisenbaum@gmail.com>
2017-09-30 16:09:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-/*var lowercase = "hello!"
-var uppercase = "HELLO!" */
+
+function sayHiToGrandma(string){
+  if (string === "hello!"){
+    return "I can't hear you!"   
+  }
+}
